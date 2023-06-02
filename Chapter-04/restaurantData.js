7bf5be7b49af4778bb08883f9d55233f78392e53
shubham225411 <shubhamkumar225411@gmail.com>
2023-06-02 23:37:36 +0530

    Chapter-04

---
+++ Chapter-04/restaurantData.js
@@ -0,0 +1,480 @@
+export const RestaurantList =[
+        {
+          "type": "restaurant",
+          "data": {
+            "type": "F",
+            "id": "36001",
+            "name": "California Burrito",
+            "uuid": "512043a1-fab0-4adb-9375-33188ec178db",
+            "city": "1",
+            "area": "HSR Layout",
+            "totalRatingsString": "10000+ ratings",
+            "cloudinaryImageId": "y5inlfbbp3v8c8warrgb",
+            "cuisines": [
+              "Mexican",
+              "American",
+              "Salads",
+              "Continental",
+              "Keto",
+              "Healthy Food"
+            ],
+            "tags": [
+              
+            ],
+            "costForTwo": 25000,
+            "costForTwoString": "₹250 FOR TWO",
+            "deliveryTime": 19,
+            "minDeliveryTime": 19,
+            "maxDeliveryTime": 19,
+            "slaString": "19 MINS",
+            "lastMileTravel": 2,
+            "slugs": {
+              "restaurant": "california-burrito-hsr-layout-hsr",
+              "city": "bangalore"
+            },
+            "cityState": "1",
+            "address": "166 E/A 14th Main Road Sector 7 HSR Layout, Near BDA Complex,100 meters from Mcdonalds towards UCO Bank, Bengaluru, Karnataka 560102",
+            "locality": "HSR Layout",
+            "parentId": 1252,
+            "unserviceable": false,
+            "veg": false,
+            "select": false,
+            "favorite": false,
+            "tradeCampaignHeaders": [
+              
+            ],
+            "ribbon": [
+              {
+                "type": "PROMOTED"
+              }
+            ],
+            "chain": [
+              
+            ],
+            "feeDetails": {
+              "fees": [
+                {
+                  "name": "distance",
+                  "fee": 2700,
+                  "message": ""
+                },
+                {
+                  "name": "time",
+                  "fee": 0,
+                  "message": ""
+                },
+                {
+                  "name": "special",
+                  "fee": 0,
+                  "message": ""
+                }
+              ],
+              "totalFees": 2700,
+              "message": "",
+              "title": "Delivery Charge",
+              "amount": "2700",
+              "icon": ""
+            },
+            "availability": {
+              "opened": true,
+              "nextOpenMessage": "",
+              "nextCloseMessage": ""
+            },
+            "longDistanceEnabled": 0,
+            "rainMode": "NONE",
+            "thirdPartyAddress": false,
+            "thirdPartyVendor": "",
+            "adTrackingID": "cid=6948366~p=1~eid=00000188-7c04-df32-0a98-1e1600a6015c",
+            "badges": {
+              "imageBased": [
+                
+              ],
+              "textBased": [
+                
+              ],
+              "textExtendedBadges": [
+                
+              ]
+            },
+            "lastMileTravelString": "2 kms",
+            "hasSurge": false,
+            "aggregatedDiscountInfoV3": {
+              "header": "20% OFF",
+              "subHeader": "UPTO ₹50",
+              "discountTag": "",
+              "headerTypeV2": 0
+            },
+            "sla": {
+              "restaurantId": "36001",
+              "deliveryTime": 19,
+              "minDeliveryTime": 19,
+              "maxDeliveryTime": 19,
+              "lastMileTravel": 2,
+              "lastMileDistance": 0,
+              "serviceability": "SERVICEABLE",
+              "rainMode": "NONE",
+              "longDistance": "NOT_LONG_DISTANCE",
+              "preferentialService": false,
+              "iconType": "EMPTY"
+            },
+            "promoted": true,
+            "avgRating": "4.4",
+            "totalRatings": 10000,
+            "new": false
+          },
+          "subtype": "basic"
+        },
+        {
+          "type": "restaurant",
+          "data": {
+            "type": "F",
+            "id": "57283",
+            "name": "Burger King",
+            "uuid": "ff073b00-d9a7-4e33-a2d6-3bd423bbed09",
+            "city": "1",
+            "area": "Hsr Layout",
+            "totalRatingsString": "10000+ ratings",
+            "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+            "cuisines": [
+              "Burgers",
+              "American"
+            ],
+            "tags": [
+              
+            ],
+            "costForTwo": 35000,
+            "costForTwoString": "₹350 FOR TWO",
+            "deliveryTime": 26,
+            "minDeliveryTime": 26,
+            "maxDeliveryTime": 26,
+            "slaString": "26 MINS",
+            "lastMileTravel": 1.899999976158142,
+            "slugs": {
+              "restaurant": "burger-king-ground-floor-hsr-layout-sectror-3-hsr",
+              "city": "bangalore"
+            },
+            "cityState": "1",
+            "address": "Burger King India Pvt Ltd, No.1081 Ground floor , HSR layout Sectror  3  Bangalore 560102",
+            "locality": "3rd Sector",
+            "parentId": 166,
+            "unserviceable": false,
+            "veg": false,
+            "select": false,
+            "favorite": false,
+            "tradeCampaignHeaders": [
+              
+            ],
+            "chain": [
+              
+            ],
+            "feeDetails": {
+              "fees": [
+                {
+                  "name": "distance",
+                  "fee": 2800,
+                  "message": ""
+                },
+                {
+                  "name": "time",
+                  "fee": 0,
+                  "message": ""
+                },
+                {
+                  "name": "special",
+                  "fee": 0,
+                  "message": ""
+                }
+              ],
+              "totalFees": 2800,
+              "message": "",
+              "title": "Delivery Charge",
+              "amount": "2800",
+              "icon": ""
+            },
+            "availability": {
+              "opened": true,
+              "nextOpenMessage": "",
+              "nextCloseMessage": ""
+            },
+            "longDistanceEnabled": 0,
+            "rainMode": "NONE",
+            "thirdPartyAddress": false,
+            "thirdPartyVendor": "",
+            "adTrackingID": "",
+            "badges": {
+              "imageBased": [
+                
+              ],
+              "textBased": [
+                
+              ],
+              "textExtendedBadges": [
+                
+              ]
+            },
+            "lastMileTravelString": "1.8 kms",
+            "hasSurge": false,
+            "aggregatedDiscountInfoV3": {
+              "header": "60% OFF",
+              "subHeader": "UPTO ₹120",
+              "discountTag": "",
+              "headerTypeV2": 0
+            },
+            "sla": {
+              "restaurantId": "57283",
+              "deliveryTime": 26,
+              "minDeliveryTime": 26,
+              "maxDeliveryTime": 26,
+              "lastMileTravel": 1.899999976158142,
+              "lastMileDistance": 0,
+              "serviceability": "SERVICEABLE",
+              "rainMode": "NONE",
+              "longDistance": "NOT_LONG_DISTANCE",
+              "preferentialService": false,
+              "iconType": "EMPTY"
+            },
+            "promoted": false,
+            "avgRating": "4.0",
+            "totalRatings": 10000,
+            "new": false
+          },
+          "subtype": "basic"
+        },
+        {
+          "type": "restaurant",
+          "data": {
+            "type": "F",
+            "id": "342032",
+            "name": "Bhartiya Jalpan",
+            "uuid": "9bd9aa90-0b97-4f24-969a-3f16c5aafc66",
+            "city": "1",
+            "area": "HSR Layout",
+            "totalRatingsString": "10000+ ratings",
+            "cloudinaryImageId": "ybfyjdu8fhlgqj6i9dlh",
+            "cuisines": [
+              "North Indian",
+              "Sweets",
+              "Desserts",
+              "Chaat"
+            ],
+            "tags": [
+              
+            ],
+            "costForTwo": 45000,
+            "costForTwoString": "₹450 FOR TWO",
+            "deliveryTime": 27,
+            "minDeliveryTime": 27,
+            "maxDeliveryTime": 27,
+            "slaString": "27 MINS",
+            "lastMileTravel": 3.799999952316284,
+            "slugs": {
+              "restaurant": "bhartiya-jalpan-hsr-hsr",
+              "city": "bangalore"
+            },
+            "cityState": "1",
+            "address": "NO.2631,27TH MAIN ROAD, 1ST SECTOR , HSR LAYOUT, BANGALORE - 560102",
+            "locality": "1st Sector",
+            "parentId": 7518,
+            "unserviceable": false,
+            "veg": true,
+            "select": false,
+            "favorite": false,
+            "tradeCampaignHeaders": [
+              
+            ],
+            "ribbon": [
+              {
+                "type": "PROMOTED"
+              }
+            ],
+            "chain": [
+              
+            ],
+            "feeDetails": {
+              "fees": [
+                {
+                  "name": "distance",
+                  "fee": 3300,
+                  "message": ""
+                },
+                {
+                  "name": "time",
+                  "fee": 0,
+                  "message": ""
+                },
+                {
+                  "name": "special",
+                  "fee": 0,
+                  "message": ""
+                }
+              ],
+              "totalFees": 3300,
+              "message": "",
+              "title": "Delivery Charge",
+              "amount": "3300",
+              "icon": ""
+            },
+            "availability": {
+              "opened": true,
+              "nextOpenMessage": "",
+              "nextCloseMessage": ""
+            },
+            "longDistanceEnabled": 0,
+            "rainMode": "NONE",
+            "thirdPartyAddress": false,
+            "thirdPartyVendor": "",
+            "adTrackingID": "cid=6948666~p=4~eid=00000188-7c04-df32-0a98-1e1700a60431",
+            "badges": {
+              "imageBased": [
+                
+              ],
+              "textBased": [
+                
+              ],
+              "textExtendedBadges": [
+                
+              ]
+            },
+            "lastMileTravelString": "3.7 kms",
+            "hasSurge": false,
+            "aggregatedDiscountInfoV3": {
+              "header": "30% OFF",
+              "subHeader": "UPTO ₹75",
+              "discountTag": "",
+              "headerTypeV2": 0
+            },
+            "sla": {
+              "restaurantId": "342032",
+              "deliveryTime": 27,
+              "minDeliveryTime": 27,
+              "maxDeliveryTime": 27,
+              "lastMileTravel": 3.799999952316284,
+              "lastMileDistance": 0,
+              "serviceability": "SERVICEABLE",
+              "rainMode": "NONE",
+              "longDistance": "NOT_LONG_DISTANCE",
+              "preferentialService": false,
+              "iconType": "EMPTY"
+            },
+            "promoted": true,
+            "avgRating": "4.1",
+            "totalRatings": 10000,
+            "new": false
+          },
+          "subtype": "basic"
+        },
+        {
+          "type": "restaurant",
+          "data": {
+            "type": "F",
+            "id": "59593",
+            "name": "Al Daaz",
+            "uuid": "c189b92c-d842-4595-9a1f-ff85bd67bc2a",
+            "city": "1",
+            "area": "Hsr Layout",
+            "totalRatingsString": "10000+ ratings",
+            "cloudinaryImageId": "rxsvhvcdip9dbfdijzk9",
+            "cuisines": [
+              "American",
+              "Arabian",
+              "Chinese",
+              "Desserts",
+              "Mughlai",
+              "North Indian"
+            ],
+            "tags": [
+              
+            ],
+            "costForTwo": 40000,
+            "costForTwoString": "₹400 FOR TWO",
+            "deliveryTime": 34,
+            "minDeliveryTime": 34,
+            "maxDeliveryTime": 34,
+            "slaString": "34 MINS",
+            "lastMileTravel": 4.300000190734863,
+            "slugs": {
+              "restaurant": "al-daaz-hsr-hsr",
+              "city": "bangalore"
+            },
+            "cityState": "1",
+            "address": "#64 & 65, 27th Main Rd, 1st Sector HSR Layout - 560102",
+            "locality": "HSR",
+            "parentId": 21640,
+            "unserviceable": false,
+            "veg": false,
+            "select": false,
+            "favorite": false,
+            "tradeCampaignHeaders": [
+              
+            ],
+            "chain": [
+              
+            ],
+            "feeDetails": {
+              "fees": [
+                {
+                  "name": "distance",
+                  "fee": 3900,
+                  "message": ""
+                },
+                {
+                  "name": "time",
+                  "fee": 0,
+                  "message": ""
+                },
+                {
+                  "name": "special",
+                  "fee": 0,
+                  "message": ""
+                }
+              ],
+              "totalFees": 3900,
+              "message": "",
+              "title": "Delivery Charge",
+              "amount": "3900",
+              "icon": ""
+            },
+            "availability": {
+              "opened": true,
+              "nextOpenMessage": "",
+              "nextCloseMessage": ""
+            },
+            "longDistanceEnabled": 0,
+            "rainMode": "NONE",
+            "thirdPartyAddress": false,
+            "thirdPartyVendor": "",
+            "adTrackingID": "",
+            "badges": {
+              "imageBased": [
+                
+              ],
+              "textBased": [
+                
+              ],
+              "textExtendedBadges": [
+                
+              ]
+            },
+            "lastMileTravelString": "4.3 kms",
+            "hasSurge": false,
+            "sla": {
+              "restaurantId": "59593",
+              "deliveryTime": 34,
+              "minDeliveryTime": 34,
+              "maxDeliveryTime": 34,
+              "lastMileTravel": 4.300000190734863,
+              "lastMileDistance": 0,
+              "serviceability": "SERVICEABLE",
+              "rainMode": "NONE",
+              "longDistance": "NOT_LONG_DISTANCE",
+              "preferentialService": false,
+              "iconType": "EMPTY"
+            },
+            "promoted": false,
+            "avgRating": "4.3",
+            "totalRatings": 10000,
+            "new": false
+          },
+          "subtype": "basic"
+        }
+];
